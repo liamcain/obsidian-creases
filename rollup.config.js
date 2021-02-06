1ef3f54080edaa8bf2692951faf3f8534207be94
Liam Cain <hi@liamca.in>
2021-02-06 12:09:12 -0500

    Cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
-import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import autoPreprocess from "svelte-preprocess";
 import { env } from "process";
 
 export default {
@@ -14,17 +12,10 @@ export default {
   },
   external: ["obsidian", "fs", "os", "path"],
   plugins: [
-    svelte({
-      emitCss: false,
-      preprocess: autoPreprocess(),
-    }),
     typescript({ sourceMap: env.env === "DEV" }),
     resolve({
       browser: true,
-      dedupe: ["svelte"],
     }),
-    commonjs({
-      include: "node_modules/**",
-    }),
+    commonjs(),
   ],
 };
